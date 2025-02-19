920bf49568a48483e0b76acdd8d945f22c105d28
mukta <muktaspersonal@gmail.com>
2025-02-19 12:40:04 +0530

    [mukta] added: repositorylayer

---
+++ RepositoryLayer/INTERFACES/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.MODEL;
+
+namespace RepositoryLayer.INTERFACES
+{
+    public interface IEmployeeRepository
+    {
+        public bool AddEmployee(AddEmployee employee);
+
+        public List<EmployeeModel> GetAllEmployee();
+
+        public bool UpdateEmployee(EmployeeModel employee);
+
+        public EmployeeModel GetEmployeeById(int? id);
+
+        public bool DeleteEmployee(int? id);
+    }
+}
