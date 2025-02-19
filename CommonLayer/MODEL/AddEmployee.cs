a7cd300c5683dc5c393b9f515e3ecbbe04af3e85
mukta <muktaspersonal@gmail.com>
2025-02-19 12:29:55 +0530

    [mukta] added: common layer

---
+++ CommonLayer/MODEL/AddEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer.MODEL
+{
+    public class AddEmployee
+    {
+        
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        public decimal Salary { get; set; }
+        public string Email { get; set; }
+        public string Designation { get; set; }
+    }
+}
---
+++ CommonLayer/MODEL/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.MODEL
+{
+    public class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+        public decimal Salary { get; set; }
+        public string Email { get; set; }
+        public string Designation { get; set; }
+    }
+}
